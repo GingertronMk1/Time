7918e1c0009a3af0f5b38332e517fecc5f75fe7c
Jack Ellis <jackellis1504@me.com>
2016-05-06 15:15:41 +0100

    Literally done until October

--- G51DBI/CW2/public_html/BackPHP/deletealbum.php
+++ G51DBI/CW2/public_html/BackPHP/deletealbum.php
@@ -14,12 +14,12 @@ if (mysqli_query($conn, $sql)) {
 }
 
 if(mysqli_query($conn, $trackSQL)) {
-  if(mysqli_query($conn, $cdSQL)) {
-    header("Location: ../albumspage.php?artID=0");
-  } else {
-      echo "Error updating record: " . mysqli_error($conn);
-      printf("<br><a href='../Updating/album.php?id=%s'>Click here to return to the previous page</a>", $idToDel);
-  };
+    if(mysqli_query($conn, $cdSQL)) {
+        header("Location: ../albumspage.php?artID=0");
+    } else {
+        echo "Error updating record: " . mysqli_error($conn);
+        printf("<br><a href='../Updating/album.php?id=%s'>Click here to return to the previous page</a>", $idToDel);
+    };
 } else {
     echo "Error updating record: " . mysqli_error($conn);
     printf("<br><a href='../Updating/album.php?id=%s'>Click here to return to the previous page</a>", $idToDel);
--- G51DBI/CW2/public_html/BackPHP/deleteartist.php
+++ G51DBI/CW2/public_html/BackPHP/deleteartist.php
@@ -9,19 +9,19 @@ $cdSQL = "DELETE FROM CD WHERE(artID=" . $idToDel . ")";
 $cdIDs = return_array($conn, "SELECT cdID FROM CD WHERE(CD.artID = " . $idToDel . ")");
 
 for($i = 0; $i < count($cdIDs); $i++){
-  $trackSQL = "DELETE FROM Track WHERE(cdID=" . $cdIDs[$i] . ")";
-  if(!mysqli_query($conn, $trackSQL)){
-      echo "Error updating record: " . mysqli_error($conn);
-      printf("<br><a href='../Updating/artist.php?id=%s'>Click here to return to the previous page</a>", $idToDel);
+    $trackSQL = "DELETE FROM Track WHERE(cdID=" . $cdIDs[$i] . ")";
+    if(!mysqli_query($conn, $trackSQL)){
+        echo "Error updating record: " . mysqli_error($conn);
+        printf("<br><a href='../Updating/artist.php?id=%s'>Click here to return to the previous page</a>", $idToDel);
     }
 };
 
 if (mysqli_query($conn, $cdSQL)) {
-  if(mysqli_query($conn, $artSQL)){
-    header("Location: ../artistspage.php");
-  } else {echo "Error updating record: " . mysqli_error($conn);
-  printf("<br><a href='../Updating/artist.php?id=%s'>Click here to return to the previous page</a>", $idToDel);
-}
+    if(mysqli_query($conn, $artSQL)){
+        header("Location: ../artistspage.php");
+    } else {echo "Error updating record: " . mysqli_error($conn);
+    printf("<br><a href='../Updating/artist.php?id=%s'>Click here to return to the previous page</a>", $idToDel);
+    }
 } else {
     echo "Error updating record: " . mysqli_error($conn);
     printf("<br><a href='../Updating/artist.php?id=%s'>Click here to return to the previous page</a>", $idToDel);
