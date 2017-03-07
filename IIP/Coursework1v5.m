58e78f79e8a255d22e4340626acca1ee1fd7f881
Jack Ellis <jackellis1504@me.com>
2017-03-07 15:45:03 +0000

    Version 5 and we have what is close enough to a winner

--- IIP/Coursework1v5.m
+++ IIP/Coursework1v5.m
@@ -1,31 +1,38 @@
-%im1 = imread(uigetfile('*.png'));
-im1 = imread('plant001_rgb.png');
+im1 = imread(uigetfile('*.png'));
+%im1 = imread('plant001_rgb.png');
 %im1 = imread('plant017_rgb.png');
 %im1 = imread('plant223_rgb.png');
 
 imageSize=size(im1);
-imageAvgDim=(imageSize(1)+imageSize(2))/2;
+imageAvgDim=round((imageSize(1)+imageSize(2))/2);
 seValue=round(sqrt(imageAvgDim/500));
 SE=strel('sphere',seValue);
+%bwThresh=round(imageAvgDim/5);
 
-im1Redness=im1(:,:,1)-(im1(:,:,3)+im1(:,:,2))/2;
 im1Greenness=im1(:,:,2)-(im1(:,:,1)+im1(:,:,3))/2;
-im1Blueness=im1(:,:,3)-(im1(:,:,2)+im1(:,:,1))/2;
 
-im1GreenBin=binarizeandprocess(im1Greenness, SE);
-im1GreenCircles=bwareaopen(im1Fin,2*imageAvgDim);
+im1GreenBin=binarizeAndProcess(im1Greenness, SE);
 
-im1RedBin=binarizeandprocess(im1Redness, SE);
+CC=bwconncomp(im1GreenBin);
+im1GreenBinSub=im1GreenBin;
+numPixels = cellfun(@numel,CC.PixelIdxList);
+[biggest,idx] = max(numPixels);
+im1GreenBinSub(CC.PixelIdxList{idx}) = 0;
+im1Fin=subtractImages(im1GreenBin,im1GreenBinSub);
+imdilate(im1Fin, SE);
+imshowpair(im1, im1Fin, 'blend');
 
-im1GreenSubRed=im1GreenBin-im1RedBin;
+function ed = erodeDilate(image, value)       % Noise reduction
+  ed=imerode(image,value);
+  ed=imdilate(ed,value);
+end
 
-subplot(2,2,1), imshow(im1Greenness);
-subplot(2,2,2), imshow(im1RedBin);
-subplot(2,2,3), imshow(im1Blueness);
-subplot(2,2,4), imshow(im1GreenSubRed);
-
-function imb = binarizeandprocess(image, value)
+function imb = binarizeAndProcess(image, value)
   imb=imbinarize(image);      % binarize and
-  imb=imerode(imb,value);     % noise reduce via
-  imb=imdilate(imb,value);    % erosion and dilation
+  imb=erodeDilate(imb, value);
+end
+
+function si = subtractImages(image1, image2)
+  si=image1-image2;
+  si(si<0)=0;
 end
