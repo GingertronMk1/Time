348666d8128d7e6d26fd5c3275d375f8b37be1d7
Jack Ellis <jackellis1504@me.com>
2017-04-26 00:55:21 +0100

    CW5 updates

--- LAC/CW5.hs
+++ LAC/CW5.hs
@@ -24,6 +24,9 @@ testSAT2 = And (And (And (Var 0) (Var 1)) (And (Var 2) (Var 3))) (And (Var 4) (N
 testSAT3 :: SAT
 testSAT3 = And (Not (Var 0)) (Var 0)
 
+testSATs :: [SAT]
+testSATs = [testSAT,testSAT2,testSAT3]
+
 evaluate :: SAT -> Assignment -> Bool
 evaluate (Var n) a = a !! n
 evaluate (Not s) a = not $ evaluate s a
@@ -40,21 +43,18 @@ varNum' (And s1 s2) is = (varNum' s1 is) ++ (varNum' s2 is)
 varNum' (Or s1 s2) is = (varNum' s1 is) ++ (varNum' s2 is)
 
 allAssign :: Int -> [Assignment]
-allAssign n = sequence $ replicate n [True, False]
+allAssign n = sequence $ replicate (n+1) [True, False]
 
 allAssignSAT :: SAT -> [Assignment]
-allAssignSAT s = allAssign $ (varNum s) + 1
+allAssignSAT s = allAssign $ varNum s
 
 satisfiable :: SAT -> Bool
-satisfiable s = or $ satisfiable' s
-
-satisfiable' :: SAT -> [Bool]
-satisfiable' s = map (evaluate s) $ allAssignSAT s
+satisfiable s = or $ map (evaluate s) (allAssignSAT s)
 
 solution :: SAT -> Maybe Assignment
-solution s = solution' trueIndex allAss
-             where allAss = allAssignSAT s
-                   trueIndex = elemIndex True $ satisfiable' s
+solution s = do i <- elemIndex True $ map (evaluate s) allAss
+                return $ allAss !! i
+                where allAss = allAssignSAT s
 
-solution' :: Maybe Int -> [a] -> Maybe a
-solution' mi as = Just (as!!) <*> mi    -- Using the <*> notation to apply indexing to a Maybe List
+solutions :: [SAT] -> [Maybe Assignment]
+solutions ss = map solution ss
