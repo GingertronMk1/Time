cee2a25fbe55b00bd45c2b5403dc5696b1025a28
Jack Ellis <jackellis1504@me.com>
2017-04-28 15:30:57 +0100

    Added a player object whose motion is controlled by the player properly

---
+++ CPP/CPP_CW2016/src/Psyje5PlayerObject.cpp
@@ -0,0 +1,174 @@
+#include "header.h"
+#include "Psyje5Main.h"
+#include "Psyje5PlayerObject.h"
+
+
+Psyje5PlayerObject::Psyje5PlayerObject(Psyje5Main* pEngine, int iMapX, int iMapY)
+	: Psyje5Object(pEngine, iMapX, iMapY)
+	, m_pMainEngine(pEngine)
+	, m_iMapX(iMapX)
+	, m_iMapY(iMapY)
+	, m_iDir(0)
+{
+	m_iStartDrawPosX = -25;
+	m_iStartDrawPosY = -25;
+
+	m_iDrawWidth = 50;
+	m_iDrawHeight = 50;
+
+	m_iPreviousScreenX = m_iCurrentScreenX = iMapX * 50 + 25 + 25;
+	m_iPreviousScreenY = m_iCurrentScreenY = iMapY * 50 + 25 + 40;
+
+	SetVisible(true);
+}
+
+
+Psyje5PlayerObject::~Psyje5PlayerObject()
+{
+}
+
+void Psyje5PlayerObject::Draw() {
+	if (!IsVisible())
+		return;
+
+	int iTick = m_pMainEngine->GetModifiedTime() / 20;
+	int iFrame = iTick % 30;
+	int iSize = 10 + iFrame;
+	if (iFrame > 15)
+		iSize = 10 + (30 - iFrame);
+
+	m_pMainEngine->DrawScreenOval(
+		m_iCurrentScreenX - iSize, m_iCurrentScreenY - iSize,
+		m_iCurrentScreenX + iSize - 1, m_iCurrentScreenY + iSize - 1,
+		0xff0000);
+
+	StoreLastScreenPositionForUndraw();
+
+};
+
+void Psyje5PlayerObject::DoUpdate(int iCurrentTime) {
+	m_iPreviousScreenX = m_iCurrentScreenX;
+	m_iPreviousScreenY = m_iCurrentScreenY;
+
+	DisplayableObject* pObject;
+	for (int iObjectId = 0;
+		(pObject = m_pMainEngine->GetDisplayableObject(iObjectId)) != NULL;
+		iObjectId++) {
+		if (pObject == this)
+			continue;
+		int iXDiff = pObject->GetXCentre() - m_iCurrentScreenX;
+		int iYDiff = pObject->GetYCentre() - m_iCurrentScreenY;
+
+		int iTick = iCurrentTime / 20;
+		int iFrame = iTick % 30;
+		int iSize = 10 + iFrame;
+		if (iFrame > 15)
+			iSize = 10 + (30 - iFrame);
+		int iSizeOther = iSize;
+
+		// Old Mate Pythagorus
+		if ((iXDiff ^ 2 + iYDiff ^ 2) < ((iSizeOther + iSize) ^ 2)) {
+			m_iMapX = 1 + rand() % 13;
+			m_iMapY = 1 + (rand() % 2) * 8;
+			m_iDir = 1;
+			m_oMover.Setup(
+				m_iMapX * 50 + 25 + 25, //m_iCurrentScreenX,
+				m_iMapY * 50 + 25 + 40, //m_iCurrentScreenY,
+				m_iMapX * 50 + 25 + 25,
+				m_iMapY * 50 + 25 + 40,
+				iCurrentTime,
+				iCurrentTime + 400 + rand() % 200);
+			m_oMover.Calculate(iCurrentTime);
+			m_iCurrentScreenX = m_oMover.GetX();
+			m_iCurrentScreenY = m_oMover.GetY();
+			RedrawObjects();
+			return;
+		}
+
+	}
+
+	// If movement has finished
+	if (m_oMover.HasMovementFinished(iCurrentTime))
+	{
+		Psyje5TileMan& tm = m_pMainEngine->GetTileManager();
+
+		// Handle any tile that we just moved onto
+		switch (tm.GetValue(m_iMapX, m_iMapY))
+		{
+		case 2:
+		case 3:
+		case 4:
+		case 5:
+		case 6:
+		case 7:
+			tm.UpdateTile(m_pMainEngine, m_iMapX, m_iMapY,
+				tm.GetValue(m_iMapX, m_iMapY) + 1);
+			break;
+		case 8:
+			tm.UpdateTile(m_pMainEngine, m_iMapX, m_iMapY, 0);
+			break;
+		}
+		/*
+		// Set off a new movement
+		switch (rand() % 10)
+		{
+		case 0: // Increase dir by 1
+			m_iDir = (m_iDir + 1) % 4;
+			break;
+		case 1: // Reduce dir by 1
+			m_iDir = (m_iDir + 3) % 4;
+			break;
+		}
+		*/
+		// Allow some control over the object by the player
+		if (m_pMainEngine->IsKeyPressed(SDLK_UP))
+			m_iDir = 0;
+		if (m_pMainEngine->IsKeyPressed(SDLK_RIGHT))
+			m_iDir = 1;
+		if (m_pMainEngine->IsKeyPressed(SDLK_DOWN))
+			m_iDir = 2;
+		if (m_pMainEngine->IsKeyPressed(SDLK_LEFT))
+			m_iDir = 3;
+
+
+		switch (tm.GetValue(
+			m_iMapX + GetXDiffForDirection(m_iDir),
+			m_iMapY + GetYDiffForDirection(m_iDir)))
+		{
+		case 0: // Passageway
+		case 2: // Pellet
+		case 3: // Pellet
+		case 4: // Pellet
+		case 5: // Pellet
+		case 6: // Pellet
+		case 7: // Pellet
+		case 8: // Pellet
+			// Allow move - set up new movement now
+			m_iMapX += GetXDiffForDirection(m_iDir);
+			m_iMapY += GetYDiffForDirection(m_iDir);
+
+			m_oMover.Setup(
+				m_iCurrentScreenX,
+				m_iCurrentScreenY,
+				m_iMapX * 50 + 25 + 25,
+				m_iMapY * 50 + 25 + 40,
+				iCurrentTime,
+				iCurrentTime + 400 + rand() % 200);
+			break;
+		case 1: // Wall
+			m_iDir = rand() % 4; // Rotate randomly
+			break;
+		}
+	}
+
+	if (!m_oMover.HasMovementFinished(iCurrentTime))
+	{
+		m_oMover.Calculate(iCurrentTime);
+		m_iCurrentScreenX = m_oMover.GetX();
+		m_iCurrentScreenY = m_oMover.GetY();
+	}
+
+	RedrawObjects();
+
+
+}
---
+++ CPP/CPP_CW2016/src/Psyje5PlayerObject.h
@@ -0,0 +1,40 @@
+#pragma once
+#include "Psyje5Object.h"
+
+class Psyje5PlayerObject :
+	public Psyje5Object
+{
+public:
+	Psyje5PlayerObject(Psyje5Main* pEngine, int iMapX, int iMapY);
+	~Psyje5PlayerObject(void);
+	void Draw();
+	void DoUpdate(int iCurrentTime);
+	int GetXDiffForDirection(int iDir){
+		switch (iDir % 4){
+		case 0: return 0;
+		case 1: return 1;
+		case 2: return 0;
+		case 3: return -1;
+		default: return 0;
+		}
+	}
+
+	int GetYDiffForDirection(int iDir){
+		switch (iDir % 4){
+		case 0: return -1; // Up
+		case 1: return 0; // Right
+		case 2: return 1; // Down
+		case 3: return 0; // Left
+		default: return 0; // Can never happen
+		}
+	}
+
+private:
+	Psyje5Main* m_pMainEngine;
+
+	MovePos m_oMover;
+	int m_iMapX;
+	int m_iMapY;
+	int m_iDir;
+};
+
