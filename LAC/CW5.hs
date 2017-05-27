17e95791a575325aa10787bc365b6e967f0f0247
Jack Ellis <jackellis1504@me.com>
2017-05-27 13:25:55 +0100

    I should stop doing this really, it's not helping anything. Regardless, made varnum not use an accumulator any more, yeah it's probably bad practise but it makes the code cleaner

--- LAC/CW5.hs
+++ LAC/CW5.hs
@@ -11,20 +11,13 @@ type Assignment = [Bool]
 baseSAT :: SAT
 baseSAT = Not (Var 0)
 
-testSAT :: SAT
-testSAT = And (Or (Var 0) (Not (Var 1))) (Not (Var 0))
-
 testAss :: Assignment
 testAss = [False, False]
 
-testSAT2 :: SAT
-testSAT2 = And (And (And (Var 0) (Var 1)) (And (Var 2) (Var 3))) (And (Var 4) (Not (Var 5)))
-
-testSAT3 :: SAT
-testSAT3 = And (Not (Var 0)) (Var 0)
-
 testSATs :: [SAT]
-testSATs = [testSAT,testSAT2,testSAT3]
+testSATs = [And (Or (Var 0) (Not (Var 1))) (Not (Var 0)),
+            And (And (And (Var 0) (Var 1)) (And (Var 2) (Var 3))) (And (Var 4) (Not (Var 5))),
+            And (Not (Var 0)) (Var 0)]
 
 evaluate :: SAT -> Assignment -> Bool
 evaluate (Var n) a = a !! n
@@ -33,22 +26,22 @@ evaluate (And s1 s2) a = (evaluate s1 a) && (evaluate s2 a)
 evaluate (Or s1 s2) a = (evaluate s1 a) || (evaluate s2 a)
 
 varNum :: SAT -> Int
-varNum s = maximum $ varNum' s []
+varNum = maximum . varNum'
 
-varNum' :: SAT -> [Int] -> [Int]
-varNum' (Var n) is = n:is
-varNum' (Not s) is = varNum' s is
-varNum' (And s1 s2) is = varNum' s1 (varNum' s2 is)
-varNum' (Or s1 s2) is = varNum' s1 (varNum' s2 is)
+varNum' :: SAT -> [Int]
+varNum' (Var n) = [n]
+varNum' (Not s) = varNum' s
+varNum' (And s1 s2) = varNum' s1 ++ varNum' s2
+varNum' (Or s1 s2) = varNum' s1 ++ varNum' s2
 
 allAssign :: Int -> [Assignment]
-allAssign n = sequence $ replicate (n+1) [True, False]
+allAssign n = sequence (replicate (n+1) [True, False])
 
 allAssignSAT :: SAT -> [Assignment]
-allAssignSAT s = allAssign $ varNum s
+allAssignSAT = allAssign . varNum
 
 satisfiable :: SAT -> Bool
-satisfiable s = or $ map (evaluate s) (allAssignSAT s)
+satisfiable s = or (map (evaluate s) (allAssignSAT s))
 
 solution :: SAT -> Maybe Assignment
 solution s = if trueSols /= [] then Just $ fst $ head trueSols
@@ -56,7 +49,7 @@ solution s = if trueSols /= [] then Just $ fst $ head trueSols
              where trueSols = filter snd (map (\x -> (x, evaluate s x)) (allAssignSAT s))
 
 solutions :: [SAT] -> [Maybe Assignment]
-solutions ss = map solution ss
+solutions = map solution
 
 soltest :: [Maybe Assignment]
 soltest = solutions testSATs
