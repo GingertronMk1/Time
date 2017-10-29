dc354dc2e71cf0618a75e7548e270a6b8f518365
Jack Ellis <jackellis1504@gmail.com>
2017-10-29 22:58:24 +0000

    Updated with static final ints for readability

---
+++ G53MDP/CW1/app/src/main/java/com/example/jack/coursework1/ColourChange.java
@@ -0,0 +1,47 @@
+package com.example.jack.coursework1;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+static final int BLACK = 0xFFFFFFFF;
+static final int RED = 0xFFFF0000;
+static final int GREEN = 0xFF00FF00;
+static final int BLUE = 0xFF0000FF;
+
+
+public class ColourChange extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_colour_change);
+    }
+
+    public void colourBlack(View v) {
+        Intent returnIntent = new Intent();
+        returnIntent.putExtra("newColour", 0xFFFFFFFF);
+        setResult(Activity.RESULT_OK, returnIntent);
+        finish();
+    }
+    public void colourRed(View v) {
+        Intent returnIntent = new Intent();
+        returnIntent.putExtra("newColour", 0xFFFF0000);
+        setResult(Activity.RESULT_OK, returnIntent);
+        finish();
+    }
+    public void colourGreen(View v) {
+        Intent returnIntent = new Intent();
+        returnIntent.putExtra("newColour", 0xFF00FF00);
+        setResult(Activity.RESULT_OK, returnIntent);
+        finish();
+    }
+    public void colourBlue(View v) {
+        Intent returnIntent = new Intent();
+        returnIntent.putExtra("newColour", 0xFF0000FF);
+        setResult(Activity.RESULT_OK, returnIntent);
+        finish();
+    }
+}
