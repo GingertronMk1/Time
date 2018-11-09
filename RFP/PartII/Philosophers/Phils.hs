dd1ec71ebf8bd176fc431ffd340e71640990767f
Jack <jackellis1504@gmail.com>
2018-11-09 14:38:46 +0000

    Moved Philosophers into its own folder

--- RFP/PartII/Philosophers/Phils.hs
+++ RFP/PartII/Philosophers/Phils.hs
@@ -5,27 +5,29 @@ import System.Random
 type Spoon = TMVar Int
 type Philosopher = String
 
-maxDelay :: Int
-maxDelay = 5
+maxDelay :: Int                   -- The maximum amount of time for which a
+maxDelay = 5                      -- Philosopher should be eating
+
+makePairs :: [a] -> [(a,a)]       -- A function to take a list of things and return
+makePairs (x:[])    = []          -- A list of tuples, the pairs of which are adjacent
+makePairs (x:y:[])  = (x,y):[]    -- List items
+makePairs (x:y:xs)  = (x,y):(makePairs (y:xs))
 
 newSpoon :: Int -> IO Spoon     -- Creating new spoons
 newSpoon n = newTMVarIO n
 
 getSpoon :: Spoon -> STM Int    -- When a philosopher wants to eat
-getSpoon f = takeTMVar f
+getSpoon f = takeTMVar f        -- They try to get a spoon
 
 retSpoon :: Spoon -> Int -> STM() -- When a philosopher wants to think
-retSpoon f i = putTMVar f i
+retSpoon f i = putTMVar f i       -- They return the spoon
 
-philNames :: [Philosopher]
+philNames :: [Philosopher]      -- Our list of philosophers
 philNames = ["Graham Hutton",
              "Steve Bagley",
              "Henrik Nilsson",
              "Thorsten Altenkirch",
              "John Garibaldi"]
-
-numPhils :: Int
-numPhils = length philNames
 
 runPhil :: Philosopher -> (Spoon, Spoon) -> IO()
 runPhil p (l, r) = do putStrLn (p ++ " is hungry")
@@ -42,10 +44,11 @@ runPhil p (l, r) = do putStrLn (p ++ " is hungry")
                       threadDelay (delay*1000000)
                       runPhil p (l, r)
 
+main :: IO()
 main = do spoons <- mapM newSpoon [1..length philNames]
           (putStrLn . show) philNames
           let namedPhils = map runPhil philNames
-              spoonPairs = zip spoons (tail . cycle $ spoons)
+              spoonPairs = makePairs ((last spoons):spoons)
               philosophersWithSpoons = zipWith ($) namedPhils spoonPairs
           putStrLn "Press enter to stop"
           mapM_ forkIO philosophersWithSpoons
