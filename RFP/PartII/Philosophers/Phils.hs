29efc9ee319226f6f960ad52fdc5e1fdd9510bb0
Jack <jackellis1504@gmail.com>
2018-12-04 13:45:17 +0000

    Narrowed lines to fit in LaTeX

--- RFP/PartII/Philosophers/Phils.hs
+++ RFP/PartII/Philosophers/Phils.hs
@@ -25,19 +25,20 @@ philNames = ["Graham Hutton",
              "John Garibaldi"]
 
 runPhil :: Philosopher -> (Spoon, Spoon) -> IO()
-runPhil p (l, r) = do putStrLn (p ++ " is hungry")
-                      (ln, rn) <- atomically $ do leftNum <- getSpoon l
-                                                  rightNum <- getSpoon r
-                                                  return (leftNum, rightNum)
-                      putStrLn (p ++ " has both spoons and is eating")
-                      delay <- randomRIO (1,maxDelay)
-                      threadDelay (delay*1000000)
-                      putStrLn (p ++ " is done eating and is thinking again")
-                      atomically $ do retSpoon l ln
-                                      retSpoon r rn
-                      delay <- randomRIO (1,maxDelay)
-                      threadDelay (delay*1000000)
-                      runPhil p (l, r)
+runPhil p (l, r)
+  = do putStrLn (p ++ " is hungry")
+       (ln, rn) <- atomically $ do leftNum <- getSpoon l
+                                   rightNum <- getSpoon r
+                                   return (leftNum, rightNum)
+       putStrLn (p ++ " has both spoons and is eating")
+       delay <- randomRIO (1,maxDelay)
+       threadDelay (delay*1000000)
+       putStrLn (p ++ " is done eating and is thinking again")
+       atomically $ do retSpoon l ln
+                       retSpoon r rn
+       delay <- randomRIO (1,maxDelay)
+       threadDelay (delay*1000000)
+       runPhil p (l, r)
 
 makePairs :: [a] -> [(a,a)]
 makePairs (x:[])    = []
@@ -45,12 +46,13 @@ makePairs (x:y:[])  = (x,y):[]
 makePairs (x:y:xs)  = (x,y):(makePairs (y:xs))
 
 main :: IO()
-main = do spoons <- mapM newSpoon [1..length philNames]
-          (putStrLn . show) philNames
-          let namedPhils = map runPhil philNames
-              spoonPairs = makePairs ((last spoons):spoons)
-              philosophersWithSpoons = zipWith ($) namedPhils spoonPairs
-          putStrLn "Press enter to stop"
-          mapM_ forkIO philosophersWithSpoons
-          getLine
-          return ()
+main
+  = do spoons <- mapM newSpoon [1..length philNames]
+       (putStrLn . show) philNames
+       let namedPhils = map runPhil philNames
+           spoonPairs = makePairs ((last spoons):spoons)
+           philsWithSpoons = zipWith ($) namedPhils spoonPairs
+       putStrLn "Press enter to stop"
+       mapM_ forkIO philsWithSpoons
+       getLine
+       return ()
