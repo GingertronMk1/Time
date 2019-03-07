cb7fce5c1a6e298be7893e120b7533e171fd1a10
Jack <jackellis1504@gmail.com>
2019-03-07 14:30:58 +0000

    Loads

--- G54/FUZ/CW/code.r
+++ G54/FUZ/CW/code.r
@@ -5,15 +5,14 @@ library(FuzzyR)
 drawfis <- function(f,n) {
   # Converting the number passed in to a string
   nString <- toString(n);
-  dir.create(nString);
   # Drawing the FIS f's surface to a png
-#  png(filename = paste(nString, "surface.png", sep="/"),
-#      width = 1000, height = 1000, units = "px", pointsize = 12,
-#      bg = "white",  res = NA,
-#      type = c("cairo", "cairo-png", "Xlib", "quartz"));
-#  gensurf(f);
+  png(filename = paste("surface", nString, ".png", sep=""),
+      width = 1000, height = 1000, units = "px", pointsize = 12,
+      bg = "white",  res = NA,
+      type = c("cairo", "cairo-png", "Xlib", "quartz"));
+  gensurf(f);
   # Drawing the FIS f's membership functions to a png
-  png(filename = paste(nString, "membershipFns.png", sep="/")
+  png(filename = paste("membershipFns", nString, ".png", sep="")
      ,width = 1000, height = 1500, units = "px", pointsize = 12
      ,bg = "white",  res = NA
      ,type = c("cairo", "cairo-png", "Xlib", "quartz"));
@@ -22,12 +21,12 @@ drawfis <- function(f,n) {
   plotmf(f, 'input',  2,  main = "Headache");
   plotmf(f, 'output', 1,  main = "Urgency");
   # Writing the FIS f's ruleset to a txt file
-  sink(file = paste(nString, "rules.txt", sep="/")
+  sink(file = paste("rules", nString, ".txt", sep="")
       ,append = FALSE
       ,type = c("output", "message")
       ,split = FALSE);
   showrule(f);
-  sink(file = paste(nString, "fis.txt", sep="/")
+  sink(file = paste("fis", nString, ".txt", sep="")
       ,append = FALSE
       ,type = c("output", "message")
       ,split = FALSE);
@@ -36,31 +35,33 @@ drawfis <- function(f,n) {
 }
 
 fillFis <- function(f) {
-  fis <- addvar(fis, 'input',   'temperature',  c(35,39));
-  fis <- addvar(fis, 'input',   'headache',     c(0,10));
-  fis <- addvar(fis, 'output',  'urgency',      c(0,10));
+  f <- addvar(f, 'input',   'temperature',  c(35,39));
+  f <- addvar(f, 'input',   'headache',     c(0,10));
+  f <- addvar(f, 'output',  'urgency',      c(0,10));
 
 # Hypothermia is below 35 degrees C. [www.nhs.uk/conditions/hypothermia/, 2019-02-21]
 # The same source states that a normal body temperature is 37 degrees C.
 # They do not have a source on hyperthermia, however it can be reasoned to be the same difference applied positively.
 # Therefore we can assume hyperthermia to set in at a body temperature of 39 degrees C.
-  fis <- addmf(fis, 'input', 1, 'hypothermic',  'gaussmf', c(0.5,35));
-  fis <- addmf(fis, 'input', 1, 'low',          'gaussmf', c(0.5,36));
-  fis <- addmf(fis, 'input', 1, 'normal',       'gaussmf', c(0.5,37));
-  fis <- addmf(fis, 'input', 1, 'high',         'gaussmf', c(0.5,38));
-  fis <- addmf(fis, 'input', 1, 'hyperthermic', 'gaussmf', c(0.5,39));
+  f <- addmf(f, 'input', 1, 'hypothermic',  'gaussmf', c(0.5,35));
+  f <- addmf(f, 'input', 1, 'low',          'gaussmf', c(0.5,36));
+  f <- addmf(f, 'input', 1, 'normal',       'gaussmf', c(0.5,37));
+  f <- addmf(f, 'input', 1, 'high',         'gaussmf', c(0.5,38));
+  f <- addmf(f, 'input', 1, 'hyperthermic', 'gaussmf', c(0.5,39));
 
-  fis <- addmf(fis, 'input', 2, 'none',         'trapmf', c(0,0,2,3));
-  fis <- addmf(fis, 'input', 2, 'mild',         'trapmf', c(2,3,4,5));
-  fis <- addmf(fis, 'input', 2, 'moderate',     'trapmf', c(4,5,6,7));
-  fis <- addmf(fis, 'input', 2, 'distressing',  'trapmf', c(6,7,8,9));
-  fis <- addmf(fis, 'input', 2, 'unbearable',   'trapmf', c(8,9,10,10));
+  f <- addmf(f, 'input', 2, 'none',         'trapmf', c(0,0,2,3));
+  f <- addmf(f, 'input', 2, 'mild',         'trapmf', c(2,3,4,5));
+  f <- addmf(f, 'input', 2, 'moderate',     'trapmf', c(4,5,6,7));
+  f <- addmf(f, 'input', 2, 'distressing',  'trapmf', c(6,7,8,9));
+  f <- addmf(f, 'input', 2, 'unbearable',   'trapmf', c(8,9,10,10));
 
-  fis <- addmf(fis, 'output', 1, 'none',      'gaussmf', c(2,0));
-  fis <- addmf(fis, 'output', 1, 'some',      'gaussmf', c(1,3.5));
-  fis <- addmf(fis, 'output', 1, 'medium',    'gaussmf', c(1,5.5));
-  fis <- addmf(fis, 'output', 1, 'priority',  'gaussmf', c(1,7.5));
-  fis <- addmf(fis, 'output', 1, 'emergency', 'gaussmf', c(1,10));
+  f <- addmf(f, 'output', 1, 'none',      'gaussmf', c(2,0));
+  f <- addmf(f, 'output', 1, 'some',      'gaussmf', c(1,3.5));
+  f <- addmf(f, 'output', 1, 'medium',    'gaussmf', c(1,5.5));
+  f <- addmf(f, 'output', 1, 'priority',  'gaussmf', c(1,7.5));
+  f <- addmf(f, 'output', 1, 'emergency', 'gaussmf', c(1,10));
+
+  return(f);
 }
 
 # SETTING UP A BASE FIS FROM WHICH THE OPTIONS WILL BE CREATED
@@ -73,11 +74,11 @@ fis <- fillFis(fis);
 # FIS 1 - ONLY CONSIDERING TEMPERATURE
 
 # RULE SET
-# 1. temperature == hypothermic => urgency = emergency
-# 2. temperature == hyperthermic => urgency = emergency
-# 3. temperature == normal => urgency = none
+# 1. temperature == hypothermic   => urgency = emergency
+# 2. temperature == hyperthermic  => urgency = emergency
+# 3. temperature == normal        => urgency = none
 
-fis1 <- fis
+fis1 <- fis;
 
 rule1_1 <- c(1,0,5,1,1);
 rule1_2 <- c(3,0,1,1,1);
@@ -90,9 +91,12 @@ fis1 <- addrule(fis1, rule1);
 # FIS 2 - ONLY CONSIDERING HEADACHE
 
 # RULE SET
-# 1. headache == none => urgency = none
-# 2. headache == unbearable => emergency
-# n. headache is directly proportional to emergency
+# 1. headache == none         => urgency = none
+# 2. headache == mild         => urgency = some
+# 3. headache == moderate     => urgency = medium
+# 4. headache == distressing  => urgency = priority
+# 5. headache == unbearable   => urgency = emergency
+
 
 fis2 <- fis
 
@@ -119,14 +123,22 @@ fis3 <- addrule(fis3, rule2);
 
 # FIS 4 - SLIGHTLY MORE REALISTIC
 
+# RULE SET
+# 1. temp == normal       && headache == none         => urgency = none
+# 2. temp == normal       && headache == moderate     => urgency = some
+# 3. temp == normal       && headache == mild         => urgency = some
+# 4. temp == normal       && headache == distressing  => urgency = medium
+# 5. temp == hypothermic  || headache == unbearable   => urgency = emergency
+# 6. temp == hyperthermic || headache == unbearable   => urgency = emergency
+
 fis4 <- fis
 
 rule4_1 <- c(3,1,1,1,1);
-rule4_2 <- c(1,5,5,1,2);
-rule4_3 <- c(5,5,5,1,2);
-rule4_4 <- c(3,3,2,1,1);
-rule4_5 <- c(3,2,2,1,1);
-rule4_6 <- c(3,4,3,1,1);
+rule4_2 <- c(3,3,2,1,1);
+rule4_3 <- c(3,2,2,1,1);
+rule4_4 <- c(3,4,3,1,1);
+rule4_5 <- c(1,5,5,1,2);
+rule4_6 <- c(5,5,5,1,2);
 
 rule4 <- rbind(rule4_1
               ,rule4_2
@@ -138,15 +150,37 @@ rule4 <- rbind(rule4_1
 
 fis4 <- addrule(fis4, rule4);
 
-# FIS 5 - AS FIS 4 BUT NOT MAMDANI
+# FIS' 5-8 - AS FIS 4 BUT DIFFERENT DEFUZZ METHODS
 
-fis5 <- newfis("Hospital"
-              ,fisType = "sugeno"
-              );
-#fis5 <- fillFis(fis5);
-#fis5 <- addrule(fis5, rule4);
+fis5 <- fillFis(newfis("Hospital"
+                      ,defuzzMethod = "bisector"
+                      )
+               );
+fis5 <- addrule(fis5, rule4);
 
-showfis(fis5);
+fis6 <- fillFis(newfis("Hospital"
+                      ,defuzzMethod = "mom"
+                      )
+               );
+fis6 <- addrule(fis6, rule4);
+
+fis7 <- fillFis(newfis("Hospital"
+                      ,defuzzMethod = "som"
+                      )
+               );
+fis7 <- addrule(fis7, rule4);
+
+fis8 <- fillFis(newfis("Hospital"
+                      ,defuzzMethod = "lom"
+                      )
+               );
+fis8 <- addrule(fis8, rule4);
+
+fis9 <- fillFis(newfis("Hospital"
+                      ,fisType="sugeno"
+                      )
+               );
+fis9 <- addrule(fis9, rule4);
 
 #DRAWING THE FISs
 
@@ -154,4 +188,8 @@ drawfis(fis1,1);
 drawfis(fis2,2);
 drawfis(fis3,3);
 drawfis(fis4,4);
-showfis(fis5);
+drawfis(fis5,5);
+drawfis(fis6,6);
+drawfis(fis7,7);
+drawfis(fis8,8);
+drawfis(fis9,9);
